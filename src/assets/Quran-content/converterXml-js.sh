153d0e81251d78924d5eaa8d55b3277d5ea605fb
moroclash <mmoroclash@gmail.com>
2020-12-30 18:05:16 +0200

    Convert Information from xml to valid JS6

--- src/assets/Quran-content/converterXml-js.sh
+++ src/assets/Quran-content/converterXml-js.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-for filename in ./*.xml; do
+for filename in ./*.js; do
     #echo $filename    #<?xml version='1.0' encoding='utf8'?>
-    newname=`echo "$filename" | sed "s/.*\///" | sed "s/.xml//"`
+    newname=`echo "$filename" | sed "s/.*\///" | sed "s/.js//"`
     mv $filename $newname.js
 
     isInFile=$(grep -c "xml" $newname.js)
-    if [ $isInFile -eq 0 ]; then
+    if [ $isInFile -eq 1 ]; then
 	#string not contained in file
 	echo "file allredy repaired"
     else
 	#string is in file at least once
-	a=$(tail -n +2 $newname.js)
-	echo "data = '$a'" > $newname.js 
+	#a=$(tail -n +2 $newname.js)
+	a=$(sed 's/data =//g' $newname.js)
+	echo -e "let data = $a \n export default data" > $newname.js
         echo "repair files done"
     fi
 done
